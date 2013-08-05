168b40ffeb9ac0eca4b660c01f2df5893400c32d
Jeremy Choo <ChooJeremy@Ymail.com>
2013-08-05 17:42:21 +0800

    Added better command line support

--- src/TowerOfHanoiSolver.java
+++ src/TowerOfHanoiSolver.java
@@ -6,7 +6,7 @@ public class TowerOfHanoiSolver
 	public static final int SECOND = 2;
 	public static final int THIRD = 3;
 
-	public static final int totalPieces = 8;
+	public static int totalPieces = 8;
 	public static final long timeToPause = 10;
 	public static boolean useNewLine;
 
@@ -16,6 +16,19 @@ public class TowerOfHanoiSolver
 	public static void main(String[] args)
 	{
 		useNewLine = args.length != 0;
+		if(args.length >= 1)
+		{
+			useNewLine = !Jeremy.isInteger(args[0]);
+			if(!useNewLine)
+			{
+				useNewLine = false;
+				totalPieces = Integer.parseInt(args[0]);
+			}
+			if(args.length >= 2)
+			{
+				useNewLine = true;
+			}
+		}
 		towerToSolve = new TowerOfHanoi(totalPieces);
 		textShower = new TextShower("Tower of Hanoi");
 		move(FIRST, THIRD, totalPieces);
